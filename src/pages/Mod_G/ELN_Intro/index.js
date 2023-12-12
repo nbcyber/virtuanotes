25a3faa8548df5f83fb9005851240003841a15c8
Nicholas Burney <strfsh2006@gmail.com>
2023-12-12 18:45:47 +0000

    init

---
+++ src/pages/Mod_G/ELN_Intro/index.js
@@ -0,0 +1,6 @@
+const ELNIntroPage = () => {
+    return (
+        <h1>The ELP intro page will go here!</h1>
+        // Floating card array of each submodule, or ordered neatly
+    );
+}
---
+++ src/pages/Mod_G/index.js
@@ -0,0 +1,6 @@
+const EnterpriseLevelNetworking = () => {
+    return (
+        <h1>The ELP home page will go here!</h1>
+        // Floating card array of each submodule, or ordered neatly
+    );
+}
